6103e133137f4af56032f0ff60c5be4d14855a9c
ketarubot <robin09v@naver.com>
2025-08-05 19:44:03 +0900

    feat :: Header 구현

---
+++ src/components/Header.jsx
@@ -0,0 +1,18 @@
+import { Link } from 'react-router-dom';
+
+function Header() {
+  return (
+    <>
+      <header>
+        <h1>Profile Card List</h1>
+      </header>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/profile/list">Card List</Link>
+        <Link to="/profile/form">Make Card</Link>
+      </nav>
+    </>
+  )
+}
+
+export default Header;
