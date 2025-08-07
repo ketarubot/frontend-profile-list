6525194ee20d102a9b9a32e3e1c811a267a7bfea
ketarubot <robin09v@naver.com>
2025-08-07 18:30:48 +0900

    refactor :: 불필요한 import 제거

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import cardData from './data/cardData';
 import Home from './pages/Home';
 import ProfileLayout from './pages/ProfileLayout';
