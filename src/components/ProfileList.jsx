656d5f2a51072b099b74482b315e83cd8d7e2b9c
ketarubot <robin09v@naver.com>
2025-08-07 17:56:12 +0900

    feat :: ProfileList 구현

---
+++ src/components/ProfileList.jsx
@@ -0,0 +1,21 @@
+import ProfileCard from './ProfileCard';
+import { useOutletContext } from 'react-router-dom';
+import '../styles/components.css';
+
+function ProfileList() {
+  const {data, setData} = useOutletContext();
+  
+
+  return (
+    <>
+      <h1 className="title">프로필 카드 목록</h1>
+      <div className="cardContainer">
+        {data.map((p, index) =>
+          <ProfileCard key={index}data={p} setData={setData} />
+        )}
+      </div>
+    </>
+  )
+}
+
+export default ProfileList;
